936dc66d837dad680723fb37e5e62e091043e99f
PointerToNextPole <pointertonextpole@gmail.com>
2020-06-26 00:35:27 +0800

    chapter 13 finished

--- src/main/java/com/ptr2nextpole/community/controller/AuthorizeController.java
+++ src/main/java/com/ptr2nextpole/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.ptr2nextpole.community.dto.AccessTokenDTO;
 import com.ptr2nextpole.community.dto.GithubUser;
 import com.ptr2nextpole.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,22 @@ public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
 
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state) {
 
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("854268c66435990498fa");
-        accessTokenDTO.setClient_secret("d3eb1d9c9032b034805f520ac19f91c64ea3fe49");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
