5a9e60387128adffa076a71687bc096fdf01a0b3
PointerToNextPole <pointertonextpole@gmail.com>
2020-06-26 03:25:06 +0800

    chapter 18 finished

--- src/main/java/com/ptr2nextpole/community/controller/IndexController.java
+++ src/main/java/com/ptr2nextpole/community/controller/IndexController.java
@@ -1,13 +1,36 @@
 package com.ptr2nextpole.community.controller;
 
+import com.ptr2nextpole.community.mapper.UserMapper;
+import com.ptr2nextpole.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null && cookies.length != 0){
+            for (Cookie cookie : cookies) {
+                if(cookie.getName().equals("token")){
+                    String token = cookie.getValue();
+                    User user = userMapper.findByToken(token);
+                    if(user != null){
+                        request.getSession().setAttribute("user", user);
+                    }
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
--- src/main/java/com/ptr2nextpole/community/mapper/UserMapper.java
+++ src/main/java/com/ptr2nextpole/community/mapper/UserMapper.java
@@ -3,6 +3,8 @@ package com.ptr2nextpole.community.mapper;
 import com.ptr2nextpole.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,4 +14,7 @@ public interface UserMapper {
     @Insert("insert into user (NAME, ACCOUNT_ID, TOKEN, GMT_CREATE, GMT_MODIFIED) " +
             "values (#{name}, #{accountId}, #{token}, #{gmtCreate}, #{gmtModified})")
     void insert(User user);
+
+    @Select("select * from USER where TOKEN = #{token}")
+    User findByToken(@Param("token") String token);
 }
