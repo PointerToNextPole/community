c83de2499c705b5777e1de526cce3b45a72052cd
PointerToNextPole <pointertonextpole@gmail.com>
2020-07-29 15:21:10 +0800

    chapter 41 finished

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -11,12 +11,17 @@ function post() {
             "type": 1
         }),
         success: function (response) {
-            if(response.code == 200){
+            if (response.code == 200) {
                 $("#comment_section").hide();
+            } else if (response.code == 2003) {
+                let isAccepted = confirm(response.message);
+                if(isAccepted){
+                    window.open("https://github.com/login/oauth/authorize?client_id=854268c66435990498fa&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                    window.localStorage.setItem("closable", "true");
+                }
             }else{
                 alert(response.message);
             }
-            console.log(response);
         },
         dataType: "json"
     });
